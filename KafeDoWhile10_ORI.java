4c79d40dfbb4fabdb668b214168e99365a83cd25
Farrell <farrellraissa4@gmail.com>
2025-10-22 15:36:06 +0700

    Percobaan3

---
+++ KafeDoWhile10_ORI.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class KafeDoWhile10_ORI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int kopi, teh, roti;
+        String namaPelanggan;
+        double totalHarga;
+        double hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+
+        do {
+            System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                break;
+            }
+
+            System.out.print("Jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("Jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("Jumlah roti: ");
+            roti = sc.nextInt();
+
+            totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total harga untuk " + namaPelanggan + " adalah Rp" + totalHarga);
+            System.out.println("Total yang Harus Dibayar: Rp "+ totalHarga);
+            sc.nextLine();
+
+        } while (true);
+
+        sc.close();
+    }
+}
